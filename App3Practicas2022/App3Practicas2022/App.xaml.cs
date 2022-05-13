928ccdcda33b06ac74b1d1982f1a678007f87e1b
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2022-05-13 16:41:31 -0300

    Agregar archivos de proyecto.

---
+++ App3Practicas2022/App3Practicas2022/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3Practicas2022
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ListaAlumnos();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
